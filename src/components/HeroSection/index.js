c92efb954270e065088747ff117920b2d7ebb76b
Rihan <100439488+DsaRihan@users.noreply.github.com>
2025-12-09 14:21:07 +0100

    modified

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import HeroBgAnimation from '../HeroBgAnimation'
+import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton } from './HeroStyle'
+import HeroImg from '../../images/IMG_6024.jpg'
+import Typewriter from 'typewriter-effect';
+import { Bio } from '../../data/constants';
+
+const HeroSection = () => {
+    return (
+        <div id="about">
+            <HeroContainer>
+                <HeroBg>
+                    <HeroBgAnimation />
+                </HeroBg>
+                <HeroInnerContainer >
+                    <HeroLeftContainer id="Left">
+                        
+                        <TextLoop>
+                            <Title> Hey, I am
+                            <Span>
+                                <Typewriter
+                                    options={{
+                                        strings: Bio.name,
+                                        autoStart: true,
+                                        loop: true,
+                                    }}
+                                />
+                            </Span>
+                            </Title> 
+                        </TextLoop>
+                        <SubTitle>{Bio.description}</SubTitle>
+                        <ResumeButton href={Bio.resume} target='display'>Check Resume</ResumeButton>
+                    </HeroLeftContainer>
+
+                    <HeroRightContainer id="Right">
+
+                        <Img src={HeroImg} alt="hero-image" />
+                    </HeroRightContainer>
+                </HeroInnerContainer>
+
+            </HeroContainer>
+        </div>
+    )
+}
+
+export default HeroSection
